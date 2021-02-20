30440142025380402b566610b9f04d7e0f95d5e9
Sar <651375+sarinth@users.noreply.github.com>
2021-02-20 11:36:49 -0600

    Create distance-to-portal.meta.js

---
+++ distance-to-portal.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @id             iitc-plugin-s2cells
+// @name           IITC plugin: Show s2 level 6, 14, 17, 19 cells with different colors
+// @author         sarinth
+// @category       Layer
+// @version        0.1.1.20210220
+// @namespace      https://github.com/sarinth/d2p
+// @updateURL      https://raw.githubusercontent.com/sarinth/s2/master/distance-to-portal.meta.js
+// @downloadURL    https://raw.githubusercontent.com/sarinth/s2/master/distance-to-portal.user.js
+// @description    IITC: Allows your current location to be set manually, then shows the distance to the selected portal. Useful when managing portal keys.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
