fd5236e4b0a4df2c26e2d564f763e2841d1ec8b5
Sar <651375+sarinth@users.noreply.github.com>
2023-05-01 15:47:31 -0500

    remove includes

--- distance-to-portal.meta.js
+++ distance-to-portal.meta.js
@@ -2,17 +2,13 @@
 // @id             iitc-plugin-distance-to-portal@jonatkins
 // @name           IITC plugin: Distance to portal
 // @category       Portal Info
-// @version        0.2.1.20220319
+// @version        0.2.2.20230501
 // @namespace      https://github.com/sarinth/d2p
 // @updateURL      https://raw.githubusercontent.com/sarinth/s2/master/distance-to-portal.meta.js
 // @downloadURL    https://raw.githubusercontent.com/sarinth/s2/master/distance-to-portal.user.js
 // @description    IITC: Allows your current location to be set manually, then shows the distance to the selected portal. Useful when managing portal keys.
-// @include        https://*.ingress.com/intel*
-// @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
 // @match          http://*.ingress.com/intel*
-// @include        https://*.ingress.com/mission/*
-// @include        http://*.ingress.com/mission/*
 // @match          https://*.ingress.com/mission/*
 // @match          http://*.ingress.com/mission/*
 // @grant          none
