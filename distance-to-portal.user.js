b9670ce0f73783639c769869d057d904206531f4
Sar <651375+sarinth@users.noreply.github.com>
2023-05-04 11:57:08 -0500

    change path from master to main

--- distance-to-portal.user.js
+++ distance-to-portal.user.js
@@ -5,8 +5,8 @@
 // @category       Portal Info
 // @version        0.2.1.20230501
 // @namespace      https://github.com/sarinth/d2p
-// @updateURL      https://raw.githubusercontent.com/sarinth/s2/master/distance-to-portal.meta.js
-// @downloadURL    https://raw.githubusercontent.com/sarinth/s2/master/distance-to-portal.user.js
+// @updateURL      https://raw.githubusercontent.com/sarinth/s2/main/distance-to-portal.meta.js
+// @downloadURL    https://raw.githubusercontent.com/sarinth/s2/main/distance-to-portal.user.js
 // @description    IITC: Allows your current location to be set manually, then shows the distance to the selected portal. Useful when managing portal keys.
 // @match          https://*.ingress.com/*
 // @match          http://*.ingress.com/*
